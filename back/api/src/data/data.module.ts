844fca559492ced233fbb54d4916427b99c8c341
waelzahir <ozahir@student.1337.ma>
2023-12-30 00:15:57 +0100

    init: express

--- back/api/src/data/data.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DataService } from './data.service';
-import { PrismaService } from '../prisma/prisma.service'
-
-@Module({
-  providers: [DataService, PrismaService],
-  exports: [PrismaService],
-})
-export class DataModule {}
