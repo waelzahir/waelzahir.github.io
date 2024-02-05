de7fd3c3e25e04607fac4be26f5390244b4cac54
waelzahir <ozahir@student.1337.ma>
2024-02-05 01:01:06 +0100

    feat: pixel sizes

--- src/Components/Loading.tsx
+++ src/Components/Loading.tsx
@@ -1,7 +1,7 @@
 export const Loading  = () => {
     return (
         <div className="wrapper">
-            <p className="text-text glitch_fixed">
+            <p className="text-text glitch_fixed md:text-[50px] sm:text-[25px] text-[20px]">
                 Loading ...
             </p>
 
