335fcb78e37d164aeb20c92bde97ecda5221a273
waelzahir <waelzahir>
2024-03-17 21:32:08 +0000

    fixed dock

--- src/componets/Dock.tsx
+++ src/componets/Dock.tsx
@@ -30,8 +30,8 @@ const DockElem = ({state} : {state : LoadedProg}) =>{
         const icon = file.type ===Progtype.github ? GetSrc((file.content as GitProject).language) : geticon(file.type)
 
     return (
-        <div className="w-16 h-16 flex justify-center items-center">
-            <img  className="w-12 h12" src={icon} alt="" />
+        <div className="w-16 h-16 min-w-16 min-h-16 flex justify-center items-center">
+            <img  className="w-12 h-12" src={icon} alt="" />
         </div>
     )
 }
@@ -39,6 +39,7 @@ const DockElem = ({state} : {state : LoadedProg}) =>{
 const Dock = ()=>
 {
     const Memory = useContext(MemProviderContext)
+    const dref = useRef<HTMLDivElement>(null)
     
     if (!Memory)
         return null    
@@ -46,8 +47,7 @@ const Dock = ()=>
         const dock = document.getElementById("dock");
         if (!dock)
           return
-        dock.style.top = (window.innerHeight - 105 ) + "px"
-        dock.style.left = ((window.innerWidth  / 2) - (dock.getBoundingClientRect().width / 2)) + "px"   
+        Resize(dock)
         window.addEventListener("resize", () => Resize(dock))
         return () =>{
           if (dock)
@@ -55,10 +55,25 @@ const Dock = ()=>
         }
     }, [Memory[0]])
     const  Programs = Array.from(Memory[0]).map(([, value]) => value)
-
+    const overflows = dref.current?.getBoundingClientRect().width === 1024 ? true : false 
+    
     return (
-        <div id="dock"  className=" bg-green-950/30 min-w-32 h-24 backdrop-blur-sm absolute rounded-3xl  flex flex-row justify-around items-center">
-            {Programs.map((state:any) => <DockElem key={state.proccess} state={state}/>)}
+        <div ref={dref} id="dock"  className=" bg-green-950/30 min-w-16 h-24 backdrop-blur-sm absolute rounded-3xl  flex flex-row  max-w-screen-lg  ">
+            {overflows ?
+                <div className="h-full w-10 bg-black">
+
+                </div>:
+                null
+            }
+            <div className="flex-1 h-full flex flex-row  items-center   overflow-x-scroll ">
+                {Programs.map((state:any) => <DockElem key={state.proccess} state={state}/>)}
+            </div>
+            {overflows ?
+                <div className="h-full w-10 bg-black">
+
+                </div>:
+                null
+            }
         </div>
     )
 }
